bad0b3528d633fbca0fe254053bbb2232a76621f
Arshdeep2402 <arsh.nike12345@gmail.com>
2024-07-18 17:18:22 +0530

    Blog Header

---
+++ app/Blog/page.js
@@ -0,0 +1,12 @@
+import BlogHeader from '@/Components/BlogHeader'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <BlogHeader />
+    </div>
+  )
+}
+
+export default page
